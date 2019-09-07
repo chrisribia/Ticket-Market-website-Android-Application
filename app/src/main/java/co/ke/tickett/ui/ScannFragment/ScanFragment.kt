043380a9c33305b0fb78eea7c8ae42b7f40e015a
chrisribia <chrisribia@gmail.com>
2019-09-07 18:47:49 +0300

    scan viewmodel

--- app/src/main/java/co/ke/tickett/ui/ScannFragment/ScanFragment.kt
+++ app/src/main/java/co/ke/tickett/ui/ScannFragment/ScanFragment.kt
@@ -24,10 +24,5 @@ class ScanFragment : Fragment() {
         return inflater.inflate(R.layout.scan_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(ScanViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
 }
--- app/src/main/java/co/ke/tickett/ui/ScannFragment/ScanViewModel.kt
+++ app/src/main/java/co/ke/tickett/ui/ScannFragment/ScanViewModel.kt
@@ -1,7 +1,33 @@
 package co.ke.tickett.ui.ScannFragment
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import co.ke.tickett.data.db.entity.Events
+import co.ke.tickett.data.repository.EventsRespository
+import co.ke.tickett.utils.Coroutines
 
-class ScanViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class ScanViewModel(
+    private val repository : EventsRespository
+) : ViewModel() {
+
+
+    private var _currentEvents= MutableLiveData<Events>()
+
+
+
+    val currentTickets: LiveData<Events>
+        get() = _currentEvents
+
+
+    fun findEmployee(qr: String): LiveData<Events> {
+        Coroutines.io{
+            repository.fetchTickets()
+        }
+        val ticket = repository.findEvent(qr)
+        ticket.observeForever {
+            _currentEvents.postValue(it)
+        }
+        return ticket
+    }
 }
