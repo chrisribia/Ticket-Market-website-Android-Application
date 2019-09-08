3183d56357b7c8bd994e50b5004c678af2e702c1
chrisribia <chrisribia@gmail.com>
2019-09-08 15:11:27 +0300

    Add Stats fragment and its viewmodel

--- app/src/main/java/co/ke/tickett/data/network/MyApi.kt
+++ app/src/main/java/co/ke/tickett/data/network/MyApi.kt
@@ -36,7 +36,7 @@ interface MyApi {
 
             return Retrofit.Builder()
                 .client(okkHttpclient)
-                .baseUrl("http://192.168.0.21/android/v1/")
+                .baseUrl("http://192.168.42.48/android/v1/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
                 .create(MyApi::class.java)
---
+++ app/src/main/java/co/ke/tickett/ui/StatFragment/StatsViewModel.kt
@@ -0,0 +1,7 @@
+package co.ke.tickett.ui.StatFragment
+
+import androidx.lifecycle.ViewModel
+
+class StatsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
