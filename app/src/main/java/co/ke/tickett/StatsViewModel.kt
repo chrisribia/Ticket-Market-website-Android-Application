629d38145c059afe63aef4573eebc392c414217f
chrisribia <chrisribia@gmail.com>
2019-09-12 10:20:30 +0300

    Add functinality to Statsviewmodel

--- app/src/main/java/co/ke/tickett/StatsViewModel.kt
+++ app/src/main/java/co/ke/tickett/StatsViewModel.kt
@@ -2,11 +2,15 @@ package co.ke.tickett
 
 import androidx.lifecycle.ViewModel
 import co.ke.tickett.data.repository.EventsRespository
+import co.ke.tickett.data.repository.SummeryRepository
+import co.ke.tickett.utils.lazyDeferred
 
 class StatsViewModel(
-    private val repository : EventsRespository
+    private val repository : SummeryRepository
 ) : ViewModel() {
 
-
+    val quotes by lazyDeferred {
+        repository.getBalance()
+    }
 
 }
