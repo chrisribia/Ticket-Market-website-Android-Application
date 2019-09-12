7abf3561261230ca97c638861b494f9e23d288ab
chrisribia <chrisribia@gmail.com>
2019-09-12 10:22:56 +0300

    fix bug

--- app/src/main/java/co/ke/tickett/ui/CodeFragment/CodeViewModelFactory.kt
+++ app/src/main/java/co/ke/tickett/ui/CodeFragment/CodeViewModelFactory.kt
@@ -4,8 +4,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import co.ke.tickett.CodeViewModel
 import co.ke.tickett.data.repository.EventsRespository
+import co.ke.tickett.data.repository.SummeryRepository
 
-class CodeViewModelFactory(private val repository: EventsRespository):
+class CodeViewModelFactory(private val repository: SummeryRepository):
 ViewModelProvider.NewInstanceFactory()
 {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
--- app/src/main/java/co/ke/tickett/ui/StatFragment/StatsViewModelfactory.kt
+++ app/src/main/java/co/ke/tickett/ui/StatFragment/StatsViewModelfactory.kt
@@ -4,10 +4,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import co.ke.tickett.StatsViewModel
 import co.ke.tickett.data.repository.EventsRespository
+import co.ke.tickett.data.repository.SummeryRepository
 
 @Suppress("UNCHECKED_CAST")
 class StatsViewModelFactory(
-    private val repository: EventsRespository
+    private val repository: SummeryRepository
 ) : ViewModelProvider.NewInstanceFactory() {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
