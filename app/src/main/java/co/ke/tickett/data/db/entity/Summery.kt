c1a78b6c47066e1b45c3bfdb4a0c030409c1a19d
chrisribia <chrisribia@gmail.com>
2019-09-12 09:40:45 +0300

    Add Summeray Dao

--- app/src/main/java/co/ke/tickett/data/db/entity/Summery.kt
+++ app/src/main/java/co/ke/tickett/data/db/entity/Summery.kt
@@ -1,2 +1,12 @@
 package co.ke.tickett.data.db.entity
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Summery(
+    @PrimaryKey(autoGenerate = false)
+    val event_name: String?,
+    val confirmed_tickets : Int?,
+    val unconfirmed_tickets : Int?
+)
