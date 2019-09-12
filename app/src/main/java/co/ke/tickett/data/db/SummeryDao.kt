2100f03262424e91db2a549b00674d5d53be01eb
chrisribia <chrisribia@gmail.com>
2019-09-12 09:44:46 +0300

    Add summery Dao Queries

---
+++ app/src/main/java/co/ke/tickett/data/db/SummeryDao.kt
@@ -0,0 +1,23 @@
+package co.ke.tickett.data.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import co.ke.tickett.data.db.entity.Summery
+
+@Dao
+interface SummeryDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun saveAllEvents(quotes : List<Summery>)
+
+
+    @Query("SELECT * FROM Summery ")
+    fun summery(qr_code: String): LiveData<Summery>
+
+
+
+
+}
